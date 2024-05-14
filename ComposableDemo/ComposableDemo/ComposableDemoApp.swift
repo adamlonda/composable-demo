b67976688ec33c7c1bfd098a5cb86eb084b79cd2
adamlonda <adamlonda@pm.me>
2024-05-14 12:24:53 +0200

    Blank project

---
+++ ComposableDemo/ComposableDemo/ComposableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableDemoApp.swift
+//  ComposableDemo
+//
+//  Created by Adam Londa on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ComposableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
