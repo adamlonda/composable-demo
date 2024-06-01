1a3f0af58ba6315515a944cc10b8e579f794f135
Adam Londa <adamlonda@pm.me>
2024-06-01 13:48:28 +0200

    Sync-up delete

--- ComposableDemo/ComposableDemo/SyncUpDetail.swift
+++ ComposableDemo/ComposableDemo/SyncUpDetail.swift
@@ -9,34 +9,55 @@ import SwiftUI
 @Reducer struct SyncUpDetail {
 
     @ObservableState struct State: Equatable {
+        @Presents var alert: AlertState<Action.Alert>?
         @Shared var syncUp: SyncUp
         @Presents var editSyncUp: SyncUpForm.State?
     }
 
     enum Action {
+        case alert(PresentationAction<Alert>)
         case cancelEditButtonTapped
         case deleteButtonTapped
         case doneEditingButtonTapped
         case editButtonTapped
         case editSyncUp(PresentationAction<SyncUpForm.Action>)
+
+        @CasePathable enum Alert {
+            case confirmButtonTapped
+        }
     }
+
+    @Dependency(\.dismiss) var dismiss
 
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
+                case .alert(.presented(.confirmButtonTapped)):
+                    @Shared(.fileStorage(.syncUps)) var syncUps: IdentifiedArrayOf<SyncUp> = []
+                    syncUps.remove(id: state.syncUp.id)
+                    return .run { _ in await dismiss() }
+
+                case .alert(.dismiss):
+                    return .none
+
                 case .cancelEditButtonTapped:
                     state.editSyncUp = nil
                     return .none
+
                 case .deleteButtonTapped:
+                    state.alert = .deleteSyncUp
                     return .none
+
                 case .doneEditingButtonTapped:
                     guard let editedSyncUp = state.editSyncUp?.syncUp else { return .none }
                     state.syncUp = editedSyncUp
                     state.editSyncUp = nil
                     return .none
+
                 case .editButtonTapped:
                     state.editSyncUp = SyncUpForm.State(syncUp: state.syncUp)
                     return .none
+
                 case .editSyncUp:
                     return .none
             }
@@ -44,6 +65,25 @@ import SwiftUI
         .ifLet(\.$editSyncUp, action: \.editSyncUp) {
             SyncUpForm()
         }
+        .ifLet(\.$alert, action: \.alert)
+    }
+}
+
+// MARK: - Reducer Convenience
+
+extension AlertState where Action == SyncUpDetail.Action.Alert {
+
+    static let deleteSyncUp = Self {
+        TextState("Delete?")
+    } actions: {
+        ButtonState(role: .destructive, action: .confirmButtonTapped) {
+            TextState("Yes")
+        }
+        ButtonState(role: .cancel) {
+            TextState("Nevermind")
+        }
+    } message: {
+        TextState("Are you sure you want to delete this meeting?")
     }
 }
 
@@ -121,6 +161,7 @@ struct SyncUpDetailView: View {
                 store.send(.editButtonTapped)
             }
         }
+        .alert($store.scope(state: \.alert, action: \.alert))
         .sheet(item: $store.scope(state: \.editSyncUp, action: \.editSyncUp)) { editSyncUpStore in
             NavigationStack {
                 SyncUpFormView(store: editSyncUpStore)
